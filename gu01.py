56d95f338f59f73a43af482fc8ac59e92734e9c0
jguang <jguang001@gmail.com>
2011-04-11 23:25:45 +0800

    sssssssssss

--- gu01.py
+++ gu01.py
@@ -10,12 +10,12 @@ link = ""
 for f in os.listdir(rootdir):
     if(f!="catalog.txt"):
       blog = codecs.open('blog/'+f,"r","utf-8")
-      str = blog.readline()
-      string+=("  -["+str(:-1)+"]");
-      link+="  ["+str+"]: blog/"+f+""
+      str1 = blog.readline()
+      string+=("  - ["+str1.strip()+"]\n");
+      link+="  ["+str1[:-2]+"]: #blog/"+f+"\n"
       blog.close()
       
-catafile.write("#"+string+"\n\n"+link)
+catafile.write(string+"\n\n"+link)
 catafile.close()
 print "======================="
 
