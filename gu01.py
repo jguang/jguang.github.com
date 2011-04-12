a87abd3a68c2a7573398fff0d03a232e61777ba6
jguang <jguang001@gmail.com>
2011-04-12 22:32:43 +0800

    aa

--- gu01.py
+++ gu01.py
@@ -9,7 +9,8 @@ string = ""
 link = ""
 for f in os.listdir(rootdir):
     if(f!="catalog.txt"):
-      blog = codecs.open('blog/'+f,"r","utf-8")
+      fpath=unicode('blog/'+f , "utf8")
+      blog = codecs.open(fpath,"r","utf-8")
       str1 = blog.readline()
       string+=("  - ["+str1.strip()+"]\n");
       link+="  ["+str1[:-2]+"]: #blog/"+f+"\n"
