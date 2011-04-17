0b679139f078e089778aa8797e7443b1e84a5507
jguang <jguang001@gmail.com>
2011-04-18 01:21:51 +0800

    sssss

--- aa.py
+++ aa.py
@@ -1,6 +1,5 @@
 #coding=utf-8
 import os,time
 TIMEFORMAT='%Y-%m-%d'
-print time.strftime(TIMEFORMAT,time.localtime(os.stat( "blog/string.txt").st_mtime))
-print time.strftime(TIMEFORMAT,time.localtime(os.stat( "blog/string.txt").st_ctime))
+print time.strftime(TIMEFORMAT,time.localtime(os.stat( "blog/time.txt").st_mtime))
 
--- gu01.py
+++ gu01.py
@@ -12,7 +12,7 @@ for f in os.listdir(rootdir):
     if(f!="catalog.txt"):
       fpath=unicode('blog/'+f , "utf8")
       blog = codecs.open(fpath,"r","utf-8")
-      fileTime=time.strftime(TIMEFORMAT,time.localtime(os.stat(fpath).st_mtime))
+      fileTime=time.strftime(TIMEFORMAT,time.localtime(os.stat(fpath).st_ctime))
       str1 = blog.readline()
       string+=("  - ["+str1.strip()+"]-"+fileTime+"\n");
       link+="  ["+str1.strip()+"]: #blog/"+f+"\n"
