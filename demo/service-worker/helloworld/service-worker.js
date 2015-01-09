0542fc77a2046c5cb98621eb1ec582aa532a8e3d
jguang <jguang001@gmail.com>
2015-01-09 10:39:27 +0800

    up

--- demo/service-worker/helloworld/service-worker.js
+++ demo/service-worker/helloworld/service-worker.js
@@ -1,1 +1,26 @@
 // This service worker doesn't actually do anything!
+// The SW will be shutdown when not in use to save memory,
+// be aware that any global state is likely to disappear
+console.log("SW startup");
+
+self.addEventListener('install', function(event) {
+  console.log("SW installed");
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("SW activated");
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log("Caught a fetch!");
+  event.respondWith(new Response("Hello world!"));
+  /*event.respondWith(
+        fetch(event.request).then(function(response) {
+            console.log('Response from network is:', response);
+            return response;
+        }).catch(function(error) {
+            console.error('Fetching failed:', error);
+            throw error;
+        })
+    );*/
+});
